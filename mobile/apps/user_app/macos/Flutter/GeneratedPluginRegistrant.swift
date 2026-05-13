e0d103f892d1a11d2a1ac4ff3660b2b3da229d6e
SAnto-spec <santosavioxavier@gmail.com>
2026-05-13 19:35:46 +0530

    Update platform-specific plugin registrants for local_auth

--- mobile/apps/user_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile/apps/user_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,14 @@ import Foundation
 
 import flutter_secure_storage_macos
 import geolocator_apple
+import local_auth_darwin
 import package_info_plus
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
